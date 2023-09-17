19ce235d3c32696ed5fd656849cbbd5862581c8b
Soumya mishra <soumya9589@gmail.com>
2023-09-18 01:04:16 +0530

    api

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const connectDB = require('./db/conn')
+
+const PORT = process.env.PORT || 5000;
+
+const products_router = require("./routes/products")
+
+// connecting data.
+require("./db/conn");
+
+app.get("/", (req, res) => {
+   res.send("Hi I am live")
+});
+
+// middleware or to set router.
+app.use("/api/products", products_router);
+
+
+const start = async (req,res) => {
+    try{
+        connectDB();
+        app.listen(PORT,() => {
+            console.log("yes i am connected");
+        })
+    }catch (error){
+        console.log(error);
+    }
+}
+start();
